d8aa7960f185e702368d5fbd4b06afe34879ec01
ethanlumenx <ethan.lumenx@gmail.com>
2026-07-29 21:28:10 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,20 @@
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tracing::{info, error};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 mod logger;
 
+
+
+
 #[tokio::main]  
-async fn main()->anyhow::Result<()> {
+async fn main() {
     // 初始化日志系统
     let _guard = logger::init_logger();
     
     info!("Starting mini-redis server");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
-    info!("Listening on {}", listener.local_addr()?);
-
-    loop {
-        let (mut socket, addr) = listener.accept().await?;
-        info!("Accepted connection from {}", addr);
-
-        tokio::task::spawn(async move {
-            info!("Handling connection from {}", addr);
-            loop {
-                let mut buf = vec![0; 1024];
-                match socket.read(&mut buf).await {
-                    Ok(0) => {
-                        info!("Connection closed");
-                    }
-                    Ok(n) => {
-                        let data = String::from_utf8_lossy(&buf[..n]);
-                        info!("Received {} bytes: {}", n, data);
-                        
-                        
-                        if data.starts_with("PING") {
-                            info!("Received PING request");
-                            let response = b"+PONG\r\n";
-
-                            if let Err(e) = socket.write_all(response).await {
-                                error!("Failed to write to socket: {}", e);
-                                break;
-                            }
-                        } else {
-                            let response = b"+OK\r\n";
-                            if let Err(e) = socket.write_all(response).await {
-                                error!("Failed to write to socket: {}", e);
-                                break;
-                            }
-                        }
-
-                    }
-                    Err(e) => {
-                        info!("Failed to read from socket: {}", e);
-                        break;
-                    }
-                }
-            }
-        });
-    
-    }
 }
+
